576976ec6adbfa21cd22ec5dd5feffcf9fbd92ee
Hendrix Gryspeerdt <95045551+hendrixgg@users.noreply.github.com>
2023-10-08 13:25:14 -0400

    created first app

---
+++ client1/App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+/**
+ * This function will accept details about a person and create an object for them.
+ * 
+ * @param {string} name The person's full name.
+ * @param {number} age The person's age in years.
+ * @param {boolean} hasBrainDamage Whether or not the person has brain damage.
+ * @returns {Object} An object containing these three properties.
+ */
+function createPerson(name, age, hasBrainDamage) {
+    return {
+        name: name,
+        age: age,
+        hasBrainDamage: hasBrainDamage,
+    }
+}
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
