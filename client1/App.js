3c0b9a2925334796590327434c5891687465f833
Hendrix Gryspeerdt <95045551+hendrixgg@users.noreply.github.com>
2023-10-14 16:04:51 -0400

    added more generalized font loading

--- client1/App.js
+++ client1/App.js
@@ -1,7 +1,7 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { useFonts } from 'expo-font'; // You can use Fonts.loadAsync instead if you want a more fine-grained control over when your fonts are loaded and before rendering.
+import { loadAsync } from 'expo-font'; // You can use Fonts.loadAsync instead if you want a more fine-grained control over when your fonts are loaded and before rendering.
 import * as SplashScreen from 'expo-splash-screen'; // This gives me control over the splash screen.
 
 /**
@@ -20,7 +20,7 @@ function createPerson(name, age, hasBrainDamage) {
     }
 }
 
-// This is done so that we can toggle the splash screen once the custom fonts have loaded.
+// Keep the splash screen visible while resources (fonts) are loaded.
 SplashScreen.preventAutoHideAsync();
 
 /**
@@ -28,19 +28,36 @@ SplashScreen.preventAutoHideAsync();
  * @returns Entry point into the app.
  */
 export default function App() {
-    const [fontsLoaded, fontError] = useFonts({
-        'Futura-Regular': require('./assets/fonts/Futura-Regular.ttf'),
-    });
+    const [appIsReady, setAppIsReady] = useState(false);
 
-    // Hides the splash screen once fonts are done loading.
+    useEffect(() => {
+        async function prepare() {
+            try {
+                // Pre-load fonts, make any API calls needed here.
+                await loadAsync({
+                    'Futura-Regular': require('./assets/fonts/Futura-Regular.ttf'),
+                });
+                // Artificiallly delay for two seconds to simulate a slow loading experience. DELETE this code if you are going to re-use it!
+                await new Promise(resolve => setTimeout(resolve, 2000));
+            } catch (e) {
+                console.warn(e);
+            } finally {
+                // Tell the application to render.
+                setAppIsReady(true);
+            }
+        }
+
+        prepare();
+    }, []);
+
     const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded || fontError) {
+        if (appIsReady) {
+            // This tells the splash screen to hide immediately! If this was called after `setAppIsReady`, then we may see a blank screen while the app is loading its initial state and rendering its first pixels. So instead, hide the splash screen once the root view has already performed layout.
             await SplashScreen.hideAsync();
         }
-    }, [fontsLoaded, fontError]);
+    }, [appIsReady]);
 
-    // Returns null if fonts are not loaded.
-    if (!fontsLoaded && !fontError) {
+    if (!appIsReady) {
         return null;
     }
 
