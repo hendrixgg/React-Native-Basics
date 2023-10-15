0f797481b8513f052f1b27e5e6f5e4fd1a484ebf
Hendrix Gryspeerdt <95045551+hendrixgg@users.noreply.github.com>
2023-10-15 13:55:04 -0400

    text-tutorial

--- client1/App.js
+++ client1/App.js
@@ -20,8 +20,9 @@ function createPerson(name, age, hasBrainDamage) {
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
+            <Text style={styles.text}>Different kind of text!</Text>
+            <Text>This is default text.</Text>
+            <StatusBar style='auto' hidden={true} />
         </View>
     );
 }
@@ -29,8 +30,15 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: 'white',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    text: {
+        fontSize: 24,
+        color: "green",
+        fontWeight: "bold",
+        fontStyle: "italic",
+        backgroundColor: "yellow"
+    }
 });
